1e75394f8d11865a307112c47390471ece3ec46a
seada1 <105821673+seada1@users.noreply.github.com>
2022-05-20 10:40:47 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+
+import Controllers.front_controller as fc
+
+app = Flask(__name__)
+cors = CORS(app)
+# Establish all the routes handled by Flask
+fc.route(app)
+
+if __name__ == '__main__':
+    app.run()
